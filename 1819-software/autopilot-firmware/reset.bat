71f4ddd6ba5286797d474fa03a4edf1cba1062f5
Henry Nester <henrynester@gmail.com>
2018-08-22 11:28:01 -0400

    Uploading over rs485 works

--- 1819-software/autopilot-firmware/reset.bat
+++ 1819-software/autopilot-firmware/reset.bat
@@ -1,8 +1,3 @@
-echo "upload.bat Start"
-
-echo "Sending reset command to autopilot board"
-echo "(42 83 01 03 02)"
 MODE %1%: BAUD=500000 PARITY=n DATA=8 STOP=1
 printf "\x42\x83\x01\x03\x02" > %1%
-
-echo "upload.bat End"
+avrdude -p m328p -c arduino -b 115200 -P %1 -v -U flash:w:%2:i
