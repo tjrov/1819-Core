5492482314ffdc78c22ef4c1fd291a30c2aecd4a
Henry Nester <henrynester@gmail.com>
2018-09-15 15:36:30 -0400

    added files

---
+++ 1819-software/GUI/GUI(18-19)/GUI(18-19)/BetterSerialPort.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlStation.Communication
+{
+    /*
+    Protocol is as follows:
+    Header(0x42)
+    Command(bit 0 determines whether request or command)
+    Length of data
+    Data
+    Checksum(XOR of length and all data bytes)
+    */
+    public class ROVMessage
+    {
+        public byte command;
+        public byte[] data;
+    }
+    //Has an IsOpenChanged event and logging capabilities
+    public class BetterSerialPort : SerialPort
+    {
+        private List<string> history; //holds transmission and receival history
+
+        public BetterSerialPort(string portName, int baudRate) : base(portName, baudRate)
+        {
+            history = new List<string>();
+        }
+        public string GetHistory()
+        {
+            //remove all tx/rx log from history stack and append to a string
+            string result = "";
+            foreach (string s in history)
+            {
+                result += s + "\n";
+            }
+            return result;
+        }
+        //throw event and log message when port opened or closed
+        public new void Open()
+        {
+            base.Open();
+            Logger.LogString(string.Format("Port {0} opened at {1} kbaud", PortName, BaudRate));
+        }
+        public new void Close()
+        {
+            //get rid of all transmissions
+            while(IsOpen && (BytesToRead > 0 || BytesToWrite > 0))
+            {
+                base.DiscardInBuffer();
+                base.DiscardOutBuffer();
+            }
+            base.Close();
+            Logger.LogString(string.Format("Port {0} closed", PortName));
+        }
+        //note each byte received
+        public new int ReadByte()
+        {
+            int result = base.ReadByte();
+            //start a new receive record
+            if (history[history.Count - 1].StartsWith("TX: "))
+            {
+                history.Add("RX: ");
+            }
+            //add to history in hex representation
+            history[history.Count - 1] += result.ToString("X2") + ",";
+            return result;
+        }
+        //note each byte transmitted
+        public new void Write(byte[] buffer, int start, int offset)
+        {
+            string temp = "TX: ";
+            foreach (byte b in buffer)
+            {
+                temp += b.ToString("X2") + ",";
+            }
+            history.Add(temp);
+            //keep history to ten items maximum
+            while(history.Count > 10)
+            {
+                history.RemoveAt(0);
+            }
+            base.Write(buffer, start, offset);
+        }
+        //custom code for rov serial comms
+        public void TransmitRequestOrCommand(ROVMessage msg)
+        {
+            //assemble header, command, length bytes at front of message
+            byte[] temp = new byte[msg.data.Length + 4];
+            temp[0] = (byte)0x42;
+            temp[1] = msg.command;
+            temp[2] = (byte)msg.data.Length;
+            //checksum starts as msg length
+            byte calculatedChecksum = (byte)msg.data.Length;
+            //add all message data in
+            for (int i = 0; i < msg.data.Length; i++)
+            {
+                temp[i + 3] = msg.data[i];
+                calculatedChecksum ^= msg.data[i];
+            }
+            //add checksum
+            temp[temp.Length - 1] = calculatedChecksum;
+
+            //send it off
+            Write(temp, 0, temp.Length);
+        }
+
+        public ROVMessage WaitReceiveData(int timeout)
+        {
+            //after sending a request for sensor data, the ROV replies with info
+            //only allow 100 ms for this to occur
+            var task = Task.Run(() => ReceiveHelper());
+            if (task.Wait(TimeSpan.FromMilliseconds(timeout)))
+            {
+                return task.Result;
+            }
+            else
+            {
+                throw new Exception("Timed out receiving data");
+            }
+            //return ReceiveHelper();
+        }
+        private ROVMessage ReceiveHelper()
+        {
+            ROVMessage msg = new ROVMessage();
+            while (ReadByte() != 0x42) ; //read in until header byte reached
+            msg.command = (byte)ReadByte();
+            msg.data = new byte[ReadByte()];
+            byte calculatedChecksum = (byte)msg.data.Length; //start calculating a checksum
+            for (int i = 0; i < msg.data.Length; i++)
+            {
+                //read in bytes one by one, calculating checksum as we go
+                msg.data[i] = (byte)ReadByte();
+                calculatedChecksum ^= msg.data[i];
+            }
+            byte actualChecksum = (byte)ReadByte();
+            if (calculatedChecksum != actualChecksum) //see if received checksum matches calculated one
+            {
+                throw new Exception(string.Format("Received corrupted data (Calculated checksum" +
+                    " of {0} did not match received {1})", calculatedChecksum, actualChecksum));
+            }
+            return msg;
+        }
+    }
+}
---
+++ 1819-software/GUI/GUI(18-19)/GUI(18-19)/SerialCommunication.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Threading;
+using System.Collections.Concurrent;
+using System.IO;
+using ControlStation.Devices;
+using System.ComponentModel;
+
+namespace ControlStation.Communication
+{
+    public class SerialCommunication : Component
+    {
+        private BetterSerialPort port;
+
+        private ConcurrentQueue<GenericAbstractDevice> devices;
+        private Thread thread;
+
+        private bool linkActive;
+        private bool shuttingDown;
+
+        public bool LinkActive
+        {
+            get
+            {
+                return linkActive;
+            }
+            set
+            {
+                if (value)
+                {
+                    if (!port.IsOpen)
+                    {
+                        port.Open();
+                    }
+                    Logger.LogString("Communication started.");
+                    if (Started != null)
+                    {
+                        Started(this, null);
+                    }
+                }
+                else
+                {
+                    if(port.IsOpen)
+                    {
+                        port.Close();
+                    }
+                    //empty queue of devices needing update
+                    while (devices.Count > 0)
+                    {
+                        devices.TryDequeue(out GenericAbstractDevice trash);
+                    }
+                    Logger.LogString("Communication stopped.");
+                    if (Stopped != null)
+                    {
+                        Stopped(this, null); //notify rest of code with event
+                    }
+                }
+                linkActive = value;
+            }
+        }
+
+        public event EventHandler<Exception> CommunicationException;
+        public event EventHandler Started, Stopped;
+        public event EventHandler TenElapsed, FiftyElapsed, ThousandElapsed;
+
+        public SerialCommunication(BetterSerialPort port) : base()
+        {
+            this.port = port;
+
+            //Communications Process
+            this.port = port;
+            //connection between UI and background threads is a queue of Devices that need updating
+            devices = new ConcurrentQueue<GenericAbstractDevice>();
+
+            //background loop runs on this thread
+            thread = new Thread(new ThreadStart(BackgroundLoop));
+            thread.SetApartmentState(ApartmentState.STA); //for UI compatibility
+            thread.IsBackground = true;
+
+            //get going
+            thread.Start();
+            //port.Open(); //error when port opened in constructor
+        }
+        public void ShutDown()
+        {
+            LinkActive = false;
+            Thread.Sleep(500);
+            shuttingDown = true;
+        }
+        //requests update of a device
+        public void QueueDeviceUpdate(GenericAbstractDevice device)
+        {
+            devices.Enqueue(device);
+        }
+        //handles communication and processing of queue
+        private void BackgroundLoop()
+        {
+            long prevTime = DateTime.Now.Ticks;
+            int thousandCount = 0;
+            int fiftyCount = 0;
+            while (!shuttingDown)
+            {
+                if (linkActive)
+                {
+                    try
+                    {
+                        //update all queued devices
+                        while (devices.Count > 0)
+                        {
+                            //send the request or command
+                            devices.TryDequeue(out GenericAbstractDevice device);
+                            port.TransmitRequestOrCommand(device.GetMessage());
+                            //if it's a sensor and needs a reply
+                            if (device.NeedsResponse)
+                            {
+                                //get the reply
+                                ROVMessage msg = port.WaitReceiveData(1000);
+                                //update the device's data on UI thread
+                                device.Invoke(new Action(() => device.UpdateData(msg)));
+                            }
+                            //redraw the device on UI thread
+                            device.Invoke(new Action(device.UpdateControls));
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        if (ex.GetType().IsAssignableFrom(typeof(System.Threading.ThreadAbortException)))
+                        {
+                            //log history before exception for debugging
+                            Logger.LogString("Start Communication Log Dump\n" + port.GetHistory() + "\nEnd Communication Log Dump");
+                            Logger.LogException(ex);
+                            //cease communication
+                            LinkActive = false;
+                            //show exception dialog
+                            if (CommunicationException != null)
+                            {
+                                CommunicationException(this, ex);
+                            }
+                        }
+                    }
+                    //fire timers if necessary
+                    if ((DateTime.Now.Ticks - prevTime) > 10 * TimeSpan.TicksPerMillisecond)
+                    {
+                        //System.Diagnostics.Debug.WriteLine(DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond);
+                        //System.Diagnostics.Debug.WriteLine(devices.Count);
+                        prevTime = DateTime.Now.Ticks;
+                        thousandCount++;
+                        fiftyCount++;
+                        if (thousandCount > 100)
+                        {
+                            thousandCount = 0;
+                            if (ThousandElapsed != null)
+                            {
+                                ThousandElapsed(this, null);
+                            }
+                        }
+                        else if (fiftyCount > 5)
+                        {
+                            fiftyCount = 0;
+                            if (FiftyElapsed != null)
+                            {
+                                FiftyElapsed(this, null);
+                            }
+                        }
+                        else
+                        {
+                            if (TenElapsed != null)
+                            {
+                                TenElapsed(this, null);
+                            }
+                        }
+                    }
+                } else
+                {
+                    Thread.Sleep(100);
+                }
+            }
+        }
+    }
+}
