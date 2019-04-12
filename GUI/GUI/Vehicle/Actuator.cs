d3f070b31e0d3e0808e14aaeff12e470f9dfe9fb
= <shreepa.parthaje@gmail.com>
2019-04-12 16:50:03 -0400

    removed commented code

--- GUI/GUI/Vehicle/Actuator.cs
+++ GUI/GUI/Vehicle/Actuator.cs
@@ -35,24 +35,8 @@ namespace GUI
          * [0][1] First ESC's speed value (-100 to 100 percent)
          * ... and so on for all 6 ESCs
          */
-        //private List<BarGraph> speeds;
         public PropulsionActuator() : base(0x81)
         {
-            /*speeds = new List<BarGraph>();
-            Dock = DockStyle.Fill;
-            for (int y = 50; y < 410; y+=120)
-            {
-                BarGraph graph = new BarGraph("Speed", "{0:##0.0}", "%", Color.Green, -100, 100, 50);
-                speeds.Add(graph);
-                graph.Location = new Point(0, y);
-                BarGraph graph2 = new BarGraph("Speed", "{0:##0.0}", "%", Color.Green, -100, 100, 50);
-                speeds.Add(graph2);
-                graph2.Location = new Point(520, y);
-                Controls.Add(graph);
-                Controls.Add(graph2);
-            }*/
-
-            //UpdateControls();
         }
 
         /*public override void UpdateControls()
@@ -78,20 +62,8 @@ namespace GUI
     }
     public class ToolsActuator : Actuator<ToolData>
     {
-        //private List<BarGraph> speeds;
         public ToolsActuator() : base(0x82)
         {
-            //speeds = new List<BarGraph>();
-            /* for(int i = 0; i < 3; i++)
-             {
-                 //BarGraph graph = new BarGraph("Speed", "{0:##0.0}", "%", Color.Green, -100, 100, 50);
-                 /speeds.Add(graph);
-                 Controls.Add(graph);
-             }
-             speeds[0].Location = new Point(300, 0);
-             speeds[1].Location = new Point(0, 0);
-             speeds[2].Location = new Point(350, 350);*/
-            //UpdateControls();
         }
 
         /*public override void UpdateControls()
@@ -115,7 +87,6 @@ namespace GUI
 
     public class ServoActuator : Actuator<ServoData>
     {
-        //private List<BarGraph> speeds;
         public ServoActuator() : base(0x84)
         {
         }
@@ -147,55 +118,8 @@ namespace GUI
     
     public class StatusActuator : Actuator<StatusActuatorData>
     {
-        /*private Button arm, reboot, upload, estop;
-        private Timer flasher;
-        private FlowLayoutPanel panel;
-        private Bitmap estopBitmap = new Bitmap(GUI.Properties.GUI_18_19_.Properties.);*/
         public StatusActuator() : base(0x83)
         {
-            /*estopBitmap.MakeTransparent(Color.White);
-            panel = new FlowLayoutPanel
-            {
-                FlowDirection = FlowDirection.TopDown,
-                Dock = DockStyle.Fill
-            };
-            arm = new Button
-            {
-                Text = "Arm",
-                BackColor = Color.Green,
-                AutoSize = true
-            };
-            arm.Click += OnArmClick;
-            reboot = new Button
-            {
-                Text = "Reboot",
-                AutoSize = true
-            };
-            reboot.Click += OnRebootClick;
-            upload = new Button
-            {
-                Text = "Upload",
-                AutoSize = true
-            };
-            upload.Click += OnUploadClick;
-            estop = new Button
-            {
-                BackColor = Color.Red,
-                BackgroundImage = estopBitmap,
-                BackgroundImageLayout = ImageLayout.Zoom,
-                Size = new Size(75, 75)
-            };
-            estop.Click += OnArmClick;
-            flasher = new Timer
-            {
-                Interval = 500,
-            };
-            flasher.Tick += OnFlasherTick;
-            panel.Controls.Add(estop);
-            panel.Controls.Add(arm);
-            panel.Controls.Add(reboot);
-            panel.Controls.Add(upload);
-            Controls.Add(panel);*/
         }
 
         protected override byte[] Convert(StatusActuatorData controlData)
@@ -205,70 +129,5 @@ namespace GUI
 
             return result;
         }
-
-        /*public override void UpdateControls()
-        {
-            //flash button for armed state
-            if (Data.Status == ROVStatus.ARMED)
-            {
-                estop.Visible = true;
-                arm.Text = "Disarm";
-                flasher.Start();
-            }
-            //solid green for disarmed state
-            else
-            {
-                estop.Visible = false;
-                arm.Text = "Arm";
-                flasher.Stop();
-                arm.BackColor = Color.Green;
-            }
-        }*/
-
-        /*private void OnFlasherTick(object sender, EventArgs e)
-        {
-            if (arm.BackColor == Color.Green)
-            {
-                arm.BackColor = Color.White;
-            }
-            else
-            {
-                arm.BackColor = Color.Green;
-            }
-        }
-
-        private void OnRebootClick(object sender, EventArgs e)
-        {
-            Data.DesiredStatus = ROVStatus.REBOOT;
-        }
-
-        private void OnUploadClick(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-            //Show dialog box with settings
-            //Send reboot command
-            //Call Visual Studio project compile and upload
-        }
-
-        private void OnArmClick(object sender, EventArgs e)
-        {
-            if (Data.DesiredStatus == ROVStatus.ARMED)
-            {
-                //send disarm command
-                Data.DesiredStatus = ROVStatus.DISARMED;
-            }
-            else
-            {
-                //arm
-                Data.DesiredStatus = ROVStatus.ARMED;
-            }
-        } 
-        protected override void OnEnabledChanged(EventArgs e)
-        {
-            base.OnEnabledChanged(e);
-            Data.DesiredStatus = ROVStatus.DISCONNECTED;
-            Data.Status = ROVStatus.DISCONNECTED;
-            UpdateControls();
-        }*/
     }
 }
