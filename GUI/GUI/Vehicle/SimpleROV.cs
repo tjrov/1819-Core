7c188eb6b07b11a8da315f7f0d302a15f1469cb1
henrynester <henrynester@gmail.com>
2019-04-18 20:10:13 -0400

    added SimpleROV.cs

---
+++ GUI/GUI/Vehicle/SimpleROV.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    public class SimpleROV
+    { 
+        private byte[] data = new byte[16];
+        private SerialPort port;
+        public SimpleROV(string portName, int baudRate)
+        {
+            port = new SerialPort(portName, baudRate);
+            port.Open();
+        }
+        //sends values to USB-to-I2C bridge
+        public void Update()
+        {
+            //send message as below:
+            //0x42 is the header
+            //next 6 bytes are thruster motor speeds, 127 to stop, 0 reverse, 255 forward
+            //next 4 bytes are manipulator motor speeds, same control scheme
+            byte[] toSend = new byte[17];
+            toSend[0] = (byte)0x42;
+            for (int i = 0; i < 16; i++) toSend[i + 1] = data[i];
+            port.Write(toSend, 0, 17);
+        }
+        //stops all motors (thrusters and manipulators)
+        public void EmergencyStop()
+        {
+            for(int i = 0; i < 16; i++)
+            {
+                data[i] = 127;
+            }
+            Update();
+        }
+        public void SetThruster(Thrusters which, byte speed)
+        {
+            data[(int)which] = speed;
+        }
+        public void SetManipulator(Manipulators which, byte speed)
+        {
+            data[(int)which] = speed;
+        }
+    }
+    public enum Thrusters
+    {
+        ForwardPort = 5,
+        ForwardStarboard = 2,
+        AftPort = 3,
+        AftStarboard = 0,
+        VerticalPort = 4,
+        VerticalStarboard = 1
+    }
+    public enum Manipulators
+    {
+        ToolOne=6,
+        ToolTwo=7,
+        ToolThree=8,
+        ToolFour=9
+    }
+}
